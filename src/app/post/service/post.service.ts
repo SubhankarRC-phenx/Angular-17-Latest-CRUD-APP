3f7bbee401e0b8c472a5a3d4d74da91b43d3323d
Subhankar Roy Choudhury <subhankar1.phenx@gmail.com>
2024-01-28 21:44:59 +0530

    modified on edit page...

--- src/app/post/service/post.service.ts
+++ src/app/post/service/post.service.ts
@@ -11,58 +11,121 @@ import { Post } from '../post';
 export class PostService {
 
 
+
   private apiURL = 'https://jsonplaceholder.typicode.com';
 
   // Use HTTPHeader for Local to server for given acces local unknown user to sever
 
-  httpOptions ={
-    headers: new HttpHeaders({
-      'Cotent-Type':'application/json'
-    })
-  }
+  // httpOptions ={
+  //   headers: new HttpHeaders({
+  //     'Cotent-Type':'application/json'
+  //   })
+  // }
+
+
+  http_headers_urlencoded = new HttpHeaders({
+    'Content-Type': 'application/x-www-form-urlencoded',
+  });
+
+  http_headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+  });
 
   constructor(
     private httpClient:HttpClient,
   ) { }
 
-  // Get All Methods
+  // Get All Methods  :-
+
+  // getAll(): Observable<any>{
+  //   return this.httpClient.get(this.apiURL+'/posts/').pipe(catchError((error:HttpErrorResponse) =>{
+  //     return throwError(error);
+  //   })
+  //   );
+  // }
+
   getAll(): Observable<any>{
-    return this.httpClient.get(this.apiURL+'/posts/').pipe(catchError((error:HttpErrorResponse) =>{
-      return throwError(error);
-    })
-    )
+    return this.httpClient.get(this.apiURL+'/posts/',
+      {
+        headers: this.http_headers_urlencoded,
+      }
+    );
   }
 
-  // Create
+
+
+
+
+  // Create :-
+
+  // create(post:Post): Observable<any>{
+  //   return this.httpClient.post(this.apiURL+'/posts/',JSON.stringify(post),this.
+  //   httpOptions).pipe(catchError((error:HttpErrorResponse) =>{
+  //     return throwError(error);
+  //   }))
+  // }
+
   create(post:Post): Observable<any>{
-    return this.httpClient.post(this.apiURL+'/posts/',JSON.stringify(post),this.
-    httpOptions).pipe(catchError((error:HttpErrorResponse) =>{
-      return throwError(error);
-    }))
+    return this.httpClient.post(this.apiURL+'/posts/',JSON.stringify(post),
+      {
+        headers: this.http_headers_urlencoded,
+      }
+    );
   }
 
-  // Find the data by ID
+  // Find the data by ID : -
+
+  // find(id:number): Observable<any>{
+  //   return this.httpClient.get(this.apiURL+'/posts/'+id).pipe(catchError((error:HttpErrorResponse) =>{
+  //     return throwError(error);
+  //   })
+  //   )
+  // }
 
   find(id:number): Observable<any>{
-    return this.httpClient.get(this.apiURL+'/posts/'+id).pipe(catchError((error:HttpErrorResponse) =>{
-      return throwError(error);
-    })
+    return this.httpClient.get(this.apiURL+'/posts/'+id,
+      {
+        headers: this.http_headers_urlencoded,
+      }
     )
   }
-  // Update data
+
+
+  // Update data :-
+
+  // update(id:number, post:Post): Observable<any>{
+  //   return this.httpClient.put(this.apiURL+'/posts/'+id, JSON.stringify(post),this.
+  //   httpOptions).pipe(catchError((error:HttpErrorResponse) =>{
+  //     return throwError(error);
+  //   }))
+
+  // }
+
+
   update(id:number, post:Post): Observable<any>{
-    return this.httpClient.put(this.apiURL+'/posts/'+id, JSON.stringify(post),this.
-    httpOptions).pipe(catchError((error:HttpErrorResponse) =>{
-      return throwError(error);
-    }))
+    return this.httpClient.put(this.apiURL+'/posts/'+id, JSON.stringify(post),
+      {
+        headers: this.http_headers_urlencoded,
+      }
+    );
 
   }
-  // Delete Data
+
+  // Delete Data :-
+
+  // delete(id:number): Observable<any>{
+  //   return this.httpClient.delete(this.apiURL+'/posts/'+id).pipe(catchError((error:HttpErrorResponse) =>{
+  //     return throwError(error);
+  //   })
+  //   )
+  // }
+
   delete(id:number): Observable<any>{
-    return this.httpClient.delete(this.apiURL+'/posts/'+id).pipe(catchError((error:HttpErrorResponse) =>{
-      return throwError(error);
-    })
-    )
+    return this.httpClient.delete(this.apiURL+'/posts/'+id,
+      {
+        headers: this.http_headers_urlencoded,
+      }
+    );
   }
 
 }
